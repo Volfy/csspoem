5e3eb7b9c24fa852ad7381cf2aecfadb16edc4f5
Sasha <volfym79@gmail.com>
2022-07-25 16:19:24 +0200

    update to current

---
+++ main.js
@@ -0,0 +1,11 @@
+const landscape_warning = document.querySelector("#landscape-warning");
+
+const check_ratio = () => {
+    if (window.innerWidth < window.innerHeight) {
+        landscape_warning.style.cssText = "display: block; position: sticky; top: 20px;";
+    } else {
+        landscape_warning.style.cssText = "display: none";
+    }
+}
+check_ratio();
+window.onresize = check_ratio;
